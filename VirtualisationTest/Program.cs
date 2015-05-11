f5109896803eed514bfb338534c9e47060284e18
Alexander Heinz <The-First-Tiger@hotmail.de>
2015-05-11 23:03:07 +0200

    Removed unused usings

--- VirtualisationTest/Program.cs
+++ VirtualisationTest/Program.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VirtualisationTest
+﻿namespace VirtualisationTest
 {
+    using System;
     using Virtualization.HyperV;
 
     class Program
